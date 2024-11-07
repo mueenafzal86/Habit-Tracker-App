c9898974c8a5bf5636d921ae99547e5ffce4a52a
mueenafzal86 <mueenafzal86@gmail.com>
2024-11-07 19:07:03 +0500

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -13,14 +13,3 @@ DATABASES = {
 
 # Configuration for MySQL
 # If you're using MySQL:
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'your_database_name',
-        'USER': 'your_database_user',
-        'PASSWORD': 'your_database_password',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
