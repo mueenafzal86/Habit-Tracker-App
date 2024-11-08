2eb0bcaee4a8d9403329ca0c472b35b62691675d
mueenafzal86 <mueenafzal86@gmail.com>
2024-11-08 15:32:15 +0500

    Update settings.py

--- Habit_Tracker/settings.py
+++ Habit_Tracker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kh6@x^%b42999#ii=hnc&dz4+j5giscrg@fh-8s4jo0@(i2ga2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.250.237.196']
+ALLOWED_HOSTS = ['34.254.183.100']
 
 
 # Application definition
